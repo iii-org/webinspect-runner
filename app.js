9be07006035427b6c026a66d326b9ddb2f3ee441
Romulus Urakagi Tsai <urakagi@gmail.com>
2021-02-05 17:11:25 +0800

    Accept web-service-name for helm chart

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const fetch = require('node-fetch');
 const parse = require('url-parse');
 const {URLSearchParams} = require('url');
 
+const webServiceName = process.env.web_service_name;
 const wiOrigin = process.env.wi_base_url;
 const apiOrigin = process.env.api_origin
-const inspectUrl = process.env.inspect_url;
+let inspectUrl = process.env.inspect_url;
+const gitUrl = process.env.git_url;
 const gitBranch = process.env.git_branch;
 const gitCommitId = process.env.git_commit_id;
 
@@ -20,13 +22,16 @@ async function doRequest(method, url, headers, bodyForm) {
   if (!headers) {
     headers = {};
   }
-  headers['Content-Type'] = 'application/json'
   const opts = {headers}
   opts.method = method
   if (bodyForm) {
     opts.body = JSON.stringify(bodyForm)
+    headers['Content-Type'] = 'application/json'
   }
-  console.log('opts=', opts.body);
+  if (verbose) {
+    console.log('url=', url);
+    console.log('opts=', opts.body);
+  }
   try {
     const response = await fetch(url, opts)
     if (!response.ok) {
@@ -37,6 +42,7 @@ async function doRequest(method, url, headers, bodyForm) {
     }
     return await response.json()
   } catch (err) {
+    console.error(await response.text());
     console.error(err.stack)
     process.exit(1)
   }
@@ -68,9 +74,49 @@ async function wiPost(path, headers, bodyForm) {
 
 // ------------------- Routine Start -------------------
 
+if (verbose) {
+  console.log('runner version 1.0.1')
+}
+
 (async () => {
-  await createScan()
+  await apiLogin()
+  await ensureInspectUrl()
 })()
+
+async function ensureInspectUrl() {
+  if (!inspectUrl) {
+    if (verbose) console.log('Retrieving repo_id...')
+    await apiGet(`/repositories/id?repository_url=${decodeURIComponent(gitUrl)}`)
+      .then(async function(json) {
+        if (verbose) console.log('resp=', json)
+        repository_id = json.data.repository_id
+        if (verbose) {
+          console.log('repo_id is ' + repository_id)
+          console.log('Retrieving inspect url...')
+        }
+        await apiGet(`/repositories/${repository_id}/branches`).then(json => {
+          if (verbose) console.log('resp=', json)
+          const branches = json.data.branch_list
+          for (let i in branches) {
+            const b = branches[i]
+            if (gitBranch != b.name) continue
+            for (let j in b.env_url) {
+              const o = b.env_url[j]
+              for (let key in o) {
+                if (key == webServiceName) {
+                  inspectUrl = o[key][0].url
+                  break
+                }
+              }
+            }
+          }
+          createScan()
+        })
+      })
+  } else {
+    createScan()
+  }
+}
 
 async function createScan() {
   const bodyForm = {
@@ -99,7 +145,6 @@ async function apiLogin() {
 }
 
 async function writeToDB(scanId) {
-  await apiLogin()
   const bodyForm = {
     scan_id: scanId,
     project_name: projectName,
